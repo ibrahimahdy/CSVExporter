9dc39d2b7ad74dc89712d2f21fdab874e5807395
Ibrahim <i.mahdy93@gmail.com>
2019-12-04 03:06:33 +0200

    fix event attributes maping

--- src/main/java/com/apromore/CSVExporterPlugin.java
+++ src/main/java/com/apromore/CSVExporterPlugin.java
@@ -1,36 +1,22 @@
 package com.apromore;
 
-import com.opencsv.CSVWriter;
-import com.opencsv.bean.StatefulBeanToCsv;
-import com.opencsv.bean.StatefulBeanToCsvBuilder;
-import org.deckfour.xes.extension.XExtension;
-import org.deckfour.xes.extension.std.XLifecycleExtension;
 import org.deckfour.xes.model.*;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 import org.zkoss.zhtml.Messagebox;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.io.Writer;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.sql.Timestamp;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
 import org.deckfour.xes.in.XUniversalParser;
-import sun.rmi.runtime.Log;
 
 public class CSVExporterPlugin {
 
     private XUniversalParser parser;
     private String myFileName;
+    private List<String> columnNames;
 
     public void readFile(String fileName) {
         myFileName = fileName;
@@ -48,70 +34,67 @@ public class CSVExporterPlugin {
 
             if(myLog.size()!=0){
                 List<XTrace> traces = myLog.iterator().next();
-                List<String> attributes = getAttributeNames(traces);
                 List<LogModel> log = createModel(traces);
-                writeCSVFile(log, attributes);
+                writeCSVFile(log);
             }
 
         }
     }
 
+    private static String CASEID = "Case ID";
+    private static String ACTIVITY = "Activity";
 
-    private List<String> getAttributeNames(List<XTrace> traces){
+    private List<LogModel> createModel(List<XTrace> traces){
+
+        HashMap<String, String> attributeList;
+        HashMap<String, String> eventAttributes;
+
+        List<LogModel> logData = new ArrayList<LogModel>();
+        String attributeValue;
 
         Set<String> listOfAttributes = new LinkedHashSet<String>();
+        columnNames = new ArrayList<String>();
+        columnNames.add(CASEID);
+        columnNames.add(ACTIVITY);
+
         for (XTrace myTrace: traces) {
             listOfAttributes.addAll(myTrace.getAttributes().keySet());
-            List<XEvent> traceEvents = myTrace;
-            for (XEvent myEvent: traceEvents) {
+
+            attributeList = new HashMap<String, String>();
+
+            for (Map.Entry<String, XAttribute> tAtt : myTrace.getAttributes().entrySet()){
+
+                attributeValue = getAttributeValue(tAtt.getValue());
+                if(tAtt.getKey().equals("concept:name")){
+                    attributeList.put(CASEID, attributeValue);
+                }else{
+                    attributeList.put(tAtt.getKey(), attributeValue);
+                }
+            }
+
+            for (XEvent myEvent: myTrace) {
+                eventAttributes = new HashMap<String, String>();
+                eventAttributes.putAll(attributeList);
                 listOfAttributes.addAll(myEvent.getAttributes().keySet());
+
+                for (Map.Entry<String, XAttribute> eAtt : myEvent.getAttributes().entrySet()){
+
+                    attributeValue = getAttributeValue(eAtt.getValue());
+                    if(eAtt.getKey().equals("concept:name")){
+                        eventAttributes.put(ACTIVITY, attributeValue);
+                    }else{
+                        eventAttributes.put(eAtt.getKey(), attributeValue);
+                    }
+                }
+
+                logData.add(new LogModel(eventAttributes));
             }
         }
 
         if(listOfAttributes.contains("concept:name")){
             listOfAttributes.remove("concept:name");
         }
-
-        return new ArrayList<String>(listOfAttributes);
-    }
-
-
-    private List<LogModel> createModel(List<XTrace> traces){
-        String caseID;
-        String activity;
-        HashMap<String, String> otherAttributes;
-
-        List<LogModel> logData = new ArrayList<LogModel>();
-        String attributeValue;
-
-        for (XTrace myTrace: traces) {
-            caseID = activity = "";
-            otherAttributes = new HashMap<String, String>();
-
-            for (Map.Entry<String, XAttribute> tAtt : myTrace.getAttributes().entrySet()){
-
-                attributeValue = getAttributeValue(tAtt.getValue());
-                if(tAtt.getKey().equals("concept:name")){
-                    caseID = attributeValue;
-                }else{
-                    otherAttributes.put(tAtt.getKey(), attributeValue);
-                }
-            }
-
-            for (XEvent myEvent: myTrace) {
-                for (Map.Entry<String, XAttribute> eAtt : myEvent.getAttributes().entrySet()){
-
-                    attributeValue = getAttributeValue(eAtt.getValue());
-                    if(eAtt.getKey().equals("concept:name")){
-                        activity = attributeValue;
-                    }else{
-                        otherAttributes.put(eAtt.getKey(), attributeValue);
-                    }
-                }
-
-                logData.add(new LogModel(caseID, activity, otherAttributes));
-            }
-        }
+        columnNames.addAll(new ArrayList<String>(listOfAttributes));
 
     return  logData;
     }
@@ -142,17 +125,14 @@ public class CSVExporterPlugin {
     }
 
 
-
-
-    private void writeCSVFile(List<LogModel> log, List<String> columnsList){
+    private void writeCSVFile(List<LogModel> log){
 
         StringBuilder sb = new StringBuilder();
         myFileName= myFileName.replaceFirst("[.][^.]+$", "") + ".csv";
         try (PrintWriter writer = new PrintWriter(new File(myFileName))) {
 
-            sb.append("Case ID, Activity,");
             String prefix = "";
-            for(String one : columnsList){
+            for(String one : columnNames){
                 sb.append(prefix);
                 prefix = ",";
                 sb.append(one);
@@ -161,13 +141,12 @@ public class CSVExporterPlugin {
 
             String columnValue;
             for (LogModel row : log) {
-                sb.append(row.getCaseID() + "," + row.getActivity()+ ",");
                 prefix = "";
-                for(String one : columnsList){
+                for(String one : columnNames){
                     sb.append(prefix);
                     prefix = ",";
 
-                    columnValue = row.getOtherAttributes().get(one);
+                    columnValue = row.getAttributeList().get(one);
                     if(columnValue != null && columnValue.trim().length() !=0){
                         sb.append(columnValue);
                     }else{
--- src/main/java/com/apromore/LogModel.java
+++ src/main/java/com/apromore/LogModel.java
@@ -1,54 +1,23 @@
 package com.apromore;
 
-import com.opencsv.bean.CsvBindByName;
-
 import java.util.HashMap;
 
 public class LogModel {
 
-	@CsvBindByName
-	private String caseID;
+	private HashMap<String, String> attributeList;
 
-	@CsvBindByName
-	private String activity;
-
-	private HashMap<String, String> otherAttributes;
-
-
-	public LogModel(){
-
-	}
-	public LogModel(String caseID, String activity, HashMap<String, String> otherAttributes) {
-		setCaseID(caseID);
-		setActivity(activity);
-		setOtherAttributes(otherAttributes);
+	public LogModel(HashMap<String, String> attributeList) {
+		setAttributeList(attributeList);
 	}
 
-
-	public void setCaseID(String ID) {
-		this.caseID = ID;
-	}
-
-	public String getCaseID() {
-		return caseID;
-	}
-
-	public void setActivity(String con) {
-		this.activity = con;
-	}
-
-	public String getActivity() {
-		return activity;
-	}
-
-    public void setOtherAttributes(HashMap<String, String> oth)
+    public void setAttributeList(HashMap<String, String> oth)
     {
-    	this.otherAttributes = oth;
+    	this.attributeList = oth;
     }
 
-    public HashMap<String, String> getOtherAttributes()
+    public HashMap<String, String> getAttributeList()
     {
-        return otherAttributes;
+        return attributeList;
     }
 
 }
